2fdc4b11892dde3f14b0d71d107d5f937a9d3b28
Protyay140 <protyayray69@gmail.com>
2023-11-14 09:02:59 +0530

    env added

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-const URL = "mongodb://127.0.0.1:27017/mern_admin";
+// const URL = "mongodb://127.0.0.1:27017/mern_admin";
+const URI = process.env.MONGODB_URI;
 const connectDb = async ()=>{
     try {
-        await mongoose.connect(URL);
+        await mongoose.connect(URI);
         console.log("database is successfully connected....");
     } catch (error) {
         console.log("failed to connect");
