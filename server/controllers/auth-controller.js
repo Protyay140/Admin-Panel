5b0904e4a3516ea13814297768a7505dd239ae4d
Protyay140 <protyayray69@gmail.com>
2023-11-15 22:31:31 +0530

    registration backened added

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,6 @@
-const home = async (req,res)=>{
+const User = require('../models/user-schema');
+
+const home = async (req, res) => {
     try {
         res.status(200).send("this is home page of our website");
     } catch (error) {
@@ -6,13 +8,27 @@ const home = async (req,res)=>{
     }
 }
 
-const register = async (req,res)=>{
+const register = async (req, res) => {
+
     try {
         console.log(req.body);
-        res.status(200).json(req.body);
+
+        const { username, email, phone, password } = req.body;
+
+        const userExist = await User.findOne({ email });
+
+        if(userExist){
+           return res.status(404).json({msg:"user already exists"});
+        }
+
+        const userCreated = await User.create({username, email, phone, password});
+
+        console.log("user is successfully created");
+        res.status(200).json({msg : userCreated});
     } catch (error) {
         console.log(error);
+        return res.status(500).json({msg:"internal server error"});
     }
 }
 
-module.exports = {home,register};
+module.exports = { home, register };
---
+++ server/models/user-schema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    phone:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    isAdmin:{
+        type:Boolean,
+        require:true,
+        default:false
+    },
+})
+
+const User = mongoose.models.users || mongoose.model('users', userSchema);
+module.exports = User;
