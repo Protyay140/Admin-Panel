4340fb39bdb91c236b20c64b7a4c9a7c746b5685
Protyay140 <protyayray69@gmail.com>
2023-12-26 23:56:46 +0530

    working on service frontend

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,5 +1,32 @@
-export const Service = () =>{
+export const Service = () => {
     return <>
-        <h1>This is Service page</h1>
+        <div className="grid grid-cols-12">
+            <div className="col-span-8 col-start-3">
+                <div className="main-section border border-red-600">
+                    <div className="header">
+                        <div className="mx-auto py-2 px-4 font-bold first-letter:text-3xl w-fit bg-green-700 text-white rounded-md">services</div>
+                        <div className="cards-section">
+                            <div className="card border border-slate-600 w-fit">
+                                <div className="card-image">
+                                    <img src="web.jpg" alt="error_loading" />
+                                </div>
+                                <div className="card-provider">
+                                    <p>provider</p>
+                                </div>
+                                <div className="card-price">
+                                    <p>provider</p>
+                                </div>
+                                <div className="card-service">
+                                    <p>service</p>
+                                </div>
+                                <div className="card-description">
+                                    <p>description</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 }
