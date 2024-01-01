044cffc4d7dfff530b27fa4a5b409ff96cce07e6
Protyay140 <protyayray69@gmail.com>
2024-01-01 23:37:04 +0530

    auth added in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import { useAuth } from "../store/auth"
+
 export const Home = () => {
+    const {user} = useAuth();
     return <>
         <div className="grid grid-cols-12">
             <div className="col-span-8 col-start-3">
-                
+                <div className="username text-center">Hey <span className="font-bold text-green-600">{user.username}🤙🏻</span></div>
             </div>
         </div>
 
